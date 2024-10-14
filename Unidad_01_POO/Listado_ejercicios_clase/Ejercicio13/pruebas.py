1424e8fc05c519df308822fbb9c521606f7473ae
JaraGH <jara@educarex.es>
2024-10-14 18:44:41 +0200

    Hasta el Ej. 13

---
+++ Unidad_01_POO/Listado_ejercicios_clase/Ejercicio13/pruebas.py
@@ -0,0 +1,15 @@
+from clases import Producto
+
+# creación de una lista de productos
+productos = [
+    Producto("tomate", "fruta", 2.3, 100),
+    Producto("patata", "verdura", 1.5, 200),
+    Producto("cebolla", "verdura", 1.8, 150),
+    Producto("manzana", "fruta", 3.2, 50),
+    Producto("pera", "fruta", 2.7, 75),
+]
+
+productos[0].caducado = True
+
+for pro in productos:
+    print(pro.__dict__)
